242925ce8e00b6ef36adf78825aedb4500810032
megaNour <alkotobnour@gmail.com>
2017-01-13 16:03:33 +0100

    Un spring framework avec un log4j ready to go

---
+++ Builder/Framework/src/main/java/hello/Application.java
@@ -0,0 +1,30 @@
+package hello;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan
+public class Application {
+    @Bean
+    MessageService mockMessageService() {
+        return new MessageService() {
+            @Override
+            public String getMessage() {
+                return "Hello megaNour !";
+            }
+        };
+    }
+    
+    public static void main(String[] args) {
+        ApplicationContext context =
+                new AnnotationConfigApplicationContext(Application.class);
+        MessagePrinter printer = context.getBean(MessagePrinter.class);
+        printer.printMessage();
+        ((ConfigurableApplicationContext) context).close();
+    }
+}
